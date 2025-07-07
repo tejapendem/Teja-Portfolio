ba512263e5d687a91bb692a3466faac626303a55
tejapendem <pendem.teja08@gmail.com>
2025-07-07 13:51:04 +0530

    Initial commit

--- src/data/constants.js
+++ src/data/constants.js
@@ -7,13 +7,13 @@ export const Bio = {
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
-  github: "https://github.com/rishavchanda",
+  github: "https://github.com/tejapendem",
   resume:
-    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
-  linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
-  twitter: "https://twitter.com/RishavChanda",
-  insta: "https://www.instagram.com/rishav_chanda/",
-  facebook: "https://www.facebook.com/rishav.chanda.165/",
+    "https://drive.google.com/file/d/1RIMlA3gTqXyKuy_97-57SbnWRsA6Hzp9/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/pendem-teja/",
+  twitter: "",
+  insta: "https://www.instagram.com/tej_3366/",
+  facebook: "",
 };
 
 export const skills = [
